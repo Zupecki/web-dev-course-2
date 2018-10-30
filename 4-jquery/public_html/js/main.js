9cbfed36e212911df43dc5d2e827f796633fd725
Michael Zupecki <michaelzupecki@gmail.com>
2018-10-30 21:17:34 +1100

    Fixed jQuery error, played with accessing style and html information for console.log

--- 4-jquery/public_html/js/main.js
+++ 4-jquery/public_html/js/main.js
@@ -1,8 +1,10 @@
 $(".shape").click(function () {
-    hideShape(self);
+    console.log($(this).css("background-color"));
+    console.log($(this).attr("class"));
+    //hideShape($( this ));
 });
 
 function hideShape(shape) {
-    console.log("CLICKED!");
+    console.log("CLICKED!" + shape);
     shape.style.display = "none";
 }
