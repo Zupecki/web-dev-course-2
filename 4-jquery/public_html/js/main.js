336a2b738fb04536d90a93c6ab23c59dea8e5eac
Michael Zupecki <michaelzupecki@gmail.com>
2018-11-02 17:42:12 +1100

    More messing with jQuery - specifically in toying with fadeIn and fadeOut methods for HTML elements; created a bunch of custom functions to swap text on button element so it's contextually toggled with another effect, and created custom fade() function that accepts 'element', 'direction(in/out)' and '[x,y]' array of text toggles.

--- 4-jquery/public_html/js/main.js
+++ 4-jquery/public_html/js/main.js
@@ -1,44 +1,79 @@
-$(".shape").click(function () {
-    //printAttributes($(this));
-    //hideShape($(this));
-    changeText($('#text-1'));
-    //changeSrc($('.iframe'));
-    changeColor($(this), true);
-});
+window.onload = function () {
+    //click .shape class
+    $(".shape").click(function () {
+        //printAttributes($(this));
+        //hideShape($(this));
+        toggleText($('#text-1'), ["CHANGED TEXT!", "OTHER TEXT!"]);
+        //changeSrc($('.iframe'));
+        //fade($(".shape"), "out");
+    });
 
-$(".shape").hover(function () {
-    changeColor($('#text-1'));
-});
+    //click button-1 id
+    $("#button-1").click(function () {
+        //check if shapes are displayed or not
+        if($(".shape").css("display") == "none") {
+            fade($(".shape"), "in", 2500); //if not displayed, fade in
+        }
+        else {
+            fade($(".shape"), "out", 2500); //else fade out
+        }
+    });
+
+    //hover .shape class
+    $(".shape").hover(function () {
+        changeColor($('#text-1'));
+        changeColor($(this), true);
+    });
+};
+
+//FUNCS
+function fade(element, direction, duration) {
+    if(direction == "out") {
+        element.fadeOut(duration, function() {
+            toggleText($("#button-1"), ["SEND SHAPES AWAY", "BRING SHAPES BACK"]);
+        });
+    }
+    else if(direction == "in") {
+        element.fadeIn(duration, function() {
+            toggleText($("#button-1"), ["SEND SHAPES AWAY", "BRING SHAPES BACK"]);
+        });
+    }
+    
+};
+
+function fadeIn(element) {
+    element.fadeIn(5000, function() {
+        $(".shape").fadeOut(5000);
+    });
+};
 
 function changeSrc(element) {
     element.attr("src", "http://www.gumroad.com");
     element.width("50%");
-}
+};
 
 function hideShape(shape) {
     console.log("CLICKED!" + shape.attr("class"));
     shape.css("display", "none");
-}
+};
 
-function changeText(text) {
-    replacement = ""
+function toggleText(element, text) {
+    currentText = element.html();
 
-    console.log(text.html());
+    console.log(element.html());
 
-    if(text.html() == "CHANGED TEXT!") {
-        replacement = "OTHER TEXT!";
+    if(currentText == text[0]) {
+        element.html(text[1]);
     }
-    else if(text.html() != "CHANGED TEXT!") {
-        replacement = "CHANGED TEXT!";
+    else if(currentText != text[0]) {
+        element.html(text[0]);
     }
-
-    text.html(replacement);
-}
+};
 
 function printAttributes(element) {
     console.log(element.css("background-color"));
     console.log(element.attr("class"));
-}
+};
 
 function changeColor(element, background=false) {
     color = getRandomColour();
@@ -48,7 +83,7 @@ function changeColor(element, background=false) {
     }
 
     element.css("color", color);
-}
+};
 
 function getRandomColour() {
     var color = "";
