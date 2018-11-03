25f0c4fb1b9f979e10b26d9b24661a092d4203eb
Michael Zupecki <michaelzupecki@gmail.com>
2018-11-03 18:47:50 +1100

    Went crazy and converted all .shape class elements on the page into custom objects that hold the element reference, and other information - this allows a shape to remember its previous position, or in the implementation it remembers its start position so it can return there on the second click. Added inputs to allow a user to dictate where the shape you click moves to, and then a second click will return it to the original position. Also added a time input so you can set how long it takes to complete. Basic as hell, but picking it up super fast considering NONE of this was in the class.

--- 4-jquery/public_html/js/main.js
+++ 4-jquery/public_html/js/main.js
@@ -1,12 +1,20 @@
 window.onload = function () {
+    //get all shape elements on page
+    var shapes = createShapes($(".shape").toArray());
+
     //click .shape class
     $(".shape").click(function () {
+        let shapeObject = getElementObject($(this), shapes);
+
+        //animate shape to given coordinates
+        shapeObject.moveTo(500, 500);
+
         //printAttributes($(this));
         //hideShape($(this));
         toggleText($('#text-1'), ["CHANGED TEXT!", "OTHER TEXT!"]);
         //changeSrc($('.iframe'));
         //fade($(".shape"), "out");
-        move($(this), 1000);
+        //move($(this), 1000);
     });
 
     //click button-1 id
@@ -27,7 +35,52 @@ window.onload = function () {
     });
 };
 
+class Shape {
+    constructor(element, shapeId, startPos, currentPos) {
+        this.element = element;
+        this.shapeId = shapeId;
+        this.startPos = startPos;
+        this.currentPos = currentPos;
+    }
+
+    moveTo(x, y) {
+        move(this, [x, y]);
+    }
+
+    setCurrentPos(currentPos) {
+        this.currentPos = currentPos;
+    }
+}
+
 //FUNCS
+//search through all objects, match to clicked id and return custom object
+function getElementObject(element, objectArray) {
+    let shape = {};
+    
+    $.each(objectArray, function (key, value) {
+        if(element.attr("id") == value.shapeId) {
+            shape = value;
+        }
+    });
+
+    return shape;
+}
+
+//cycle through all shape elements, create new shape object and add to array
+function createShapes(elementsArray) {
+    let shapeObjects = [];
+
+    $.each(elementsArray, function (key, value) {
+        //get current position, based on auto doc placement
+        position = getPos($(value));
+
+        //create new object
+        shapeObjects.push(new Shape($(value), $(value).attr("id"), position, position));
+    });
+
+    return shapeObjects;
+}
+
 function fade(element, direction, duration) {
     if(direction == "out") {
         element.fadeOut(duration, function() {
@@ -41,42 +94,67 @@ function fade(element, direction, duration) {
     }
 };
 
-function move(element, duration) {
+//return current position
+function getPos(element) {
+    let position = element.offset();
+
+    return position;
+};
+
+function getInputCoords() {
+    let x = parseInt($("#x-input").val());
+    let y = parseInt($("#y-input").val());
+
+    return [x, y]
+}
+
+function getDuration() {
+    let duration = parseInt($("#time-input").val());
+
+    return duration
+}
+
+//set position css to absolute and current position
+function setPos(element) {
+    //get current position to set so absolute switch won't move
+    var setPos = element.offset();
+
+    //set to current position, but absolute (don't move)
+    element.css("position", "absolute");
+    element.css(setPos);
+}
+
+function move(object, moveTo) {
     let grow = 0;
-    let movePos = "0px";
+    let animProps = {};
+    //fetch coordinates and duration inputs by user from input fields on page
+    let destinationCoords = getInputCoords();
+    let duration = getDuration();
 
-    var animProps = {
-        top:"1000px",
-        left:"500px",
-        width:element.css("width"),
-        height:element.css("height")
-    };
 
-    element.css("position","absolute");
-    console.log("WIDTHx2: "+parseInt(element.css("width"))*2);
-
-    if(element.css("top") == "1000px") {
-        movePos = "0px";
-        grow = String(parseInt(element.css("width"))/2)+"px";
-    }
-    if(element.css("top") == "0px"){
-        movePos = "1000px";
-        grow = String(parseInt(element.css("width"))*2)+"px";
+    //get element from object
+    let element = object.element;
+    //set position to current position to give illusion it stays where it is on absolute switch
+    setPos(element);
+  
+    //figure out if coming or going - if current location same as start, set to new location
+    if(object.currentPos == object.startPos) {
+        animProps["left"] = destinationCoords[0];
+        animProps["top"] = destinationCoords[1];
+        //animProps["left"] = moveTo[0];
+        //animProps["top"] = moveTo[1];
     }
 
-    console.log("GROW: "+grow);
+    //figure out if coming or going - if current location different to start, set start location
+    if(object.currentPos != object.startPos){
+        animProps = object.startPos;
+    }
 
-    //update values
-    animProps["top"] = movePos;
-    animProps["left"] = movePos;
-    animProps["width"] = grow;
-    animProps["height"] = grow;
-
+    //animate based on decided qualities
     element.animate(animProps, duration, function (){
-        //if shape has returned to start pos, set back to relative
-        if(element.css("top") == "0px") {
-            element.css("position","relative");
-        } 
+        //set currentPos to position moved to
+        object.setCurrentPos(animProps);
+        console.log("ANIMATION CALLBACK");
     });
 };
 
@@ -120,7 +198,6 @@ function changeColor(element, background=false) {
     if(background == true) {
         element.css("background-color", color);
     }
-
     element.css("color", color);
 };
 
